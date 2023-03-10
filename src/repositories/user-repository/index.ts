5de402b7fce82d1ad673f751cceb6d3392c70102
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-03-10 11:43:29 -0300

    feat: sign in with GitHub

--- src/repositories/user-repository/index.ts
+++ src/repositories/user-repository/index.ts
@@ -1,5 +1,6 @@
-import { prisma } from "@/config";
-import { Prisma } from "@prisma/client";
+import { prisma } from '@/config';
+import { Prisma } from '@prisma/client';
+import axios from 'axios';
 
 async function findByEmail(email: string, select?: Prisma.UserSelect) {
   const params: Prisma.UserFindUniqueArgs = {
@@ -21,9 +22,44 @@ async function create(data: Prisma.UserUncheckedCreateInput) {
   });
 }
 
+async function exchangeCodeForAccessToken(code: string) {
+  const { REDIRECT_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
+
+  const body = {
+    code,
+    grant_type: 'authorization_code',
+    redirect_uri: REDIRECT_URL,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+  };
+
+  const { data } = await axios.post(process.env.GITHUB_ACCESS_TOKEN_URL, body, {
+    headers: {
+      Content_type: 'application/json',
+    },
+  });
+
+  const queryParams = new URLSearchParams(data);
+  const accessToken = queryParams.get('access_token');
+
+  return accessToken;
+}
+
+async function fetchUser(token: string) {
+  const response = await axios.get(process.env.GITHUB_USER_URL, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
 const userRepository = {
   findByEmail,
   create,
+  exchangeCodeForAccessToken,
+  fetchUser,
 };
 
 export default userRepository;
--- src/schemas/authentication-schemas.ts
+++ src/schemas/authentication-schemas.ts
@@ -1,7 +1,12 @@
-import { SignInParams } from "@/services";
-import Joi from "joi";
+import { CodeSchema } from '@/protocols';
+import { SignInParams } from '@/services';
+import Joi from 'joi';
 
 export const signInSchema = Joi.object<SignInParams>({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
+});
+
+export const codeSchema = Joi.object<CodeSchema>({
+  code: Joi.string().required(),
 });
