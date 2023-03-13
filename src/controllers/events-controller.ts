f1d04e9b96e41d67547a18f1fee959a1cd56ad8e
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-03-13 10:09:41 -0300

    refactor: redis config

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -1,4 +1,4 @@
-import { redis } from '@/app';
+import { conectRedis } from '@/config/redis';
 import eventsService from '@/services/events-service';
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
@@ -6,14 +6,14 @@ import httpStatus from 'http-status';
 export async function getDefaultEvent(_req: Request, res: Response) {
   const cachekey = 'event';
   try {
-    const cachedEvent = await redis.get(cachekey);
+    const cachedEvent = await conectRedis.get(cachekey);
 
     if (cachedEvent) {
       return res.send(JSON.parse(cachedEvent));
     }
 
     const event = await eventsService.getFirstEvent();
-    redis.set(cachekey, JSON.stringify(event));
+    conectRedis.set(cachekey, JSON.stringify(event));
     return res.status(httpStatus.OK).send(event);
   } catch (error) {
     return res.status(httpStatus.NOT_FOUND).send({});
