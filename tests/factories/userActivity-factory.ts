9c170c0b10c0859d615a799dc34ca840afc49586
Gabriel Portela Gomes <gabrielgomesportela@gmail.com>
2023-03-18 11:07:17 -0300

    feat: post activity route integration tests

--- tests/factories/userActivity-factory.ts
+++ tests/factories/userActivity-factory.ts
@@ -1,10 +1,41 @@
 import { prisma } from '@/config';
+import faker from '@faker-js/faker';
 
 export async function createActivity(activityTypeId: number, enrollmentId: number) {
   return prisma.activity.create({
     data: {
       activityTypeId,
       enrollmentId,
+    },
+  });
+}
+
+export async function createActivityTypeWithFewVacancies() {
+  return prisma.activityType.create({
+    data: {
+      name: faker.name.findName(),
+      schedules: `${faker.datatype.number({ min: 10, max: 15 })}:${faker.datatype.number({
+        min: 10,
+        max: 59,
+      })}-${faker.datatype.number({ min: 16, max: 20 })}:${faker.datatype.number({ min: 10, max: 59 })}`,
+      capacity: faker.datatype.number({ min: 2, max: 2 }),
+      activityDate: `${faker.date.weekday()}, ${faker.datatype.number({ min: 10, max: 28 })}/${faker.datatype.number({
+        min: 10,
+        max: 12,
+      })}`,
+      place: faker.address.streetAddress(),
+    },
+  });
+}
+
+export async function createConflictActivityType(schedule: string, activityDate: string) {
+  return prisma.activityType.create({
+    data: {
+      name: faker.name.findName(),
+      schedules: schedule,
+      capacity: faker.datatype.number({ min: 10, max: 20 }),
+      activityDate: activityDate,
+      place: faker.address.streetAddress(),
     },
   });
 }
--- tests/integration/userActivity.test.ts
+++ tests/integration/userActivity.test.ts
@@ -9,18 +9,17 @@ import supertest from 'supertest';
 import {
   createEnrollmentWithAddress,
   createUser,
-  createTicketType,
   createTicket,
   createPayment,
-  generateCreditCardData,
   createTicketTypeWithHotel,
   createTicketTypeRemote,
-  createHotel,
-  createRoomWithHotelId,
-  createBooking,
   createActivityType,
 } from '../factories';
-import { createActivity } from '../factories/userActivity-factory';
+import {
+  createActivity,
+  createActivityTypeWithFewVacancies,
+  createConflictActivityType,
+} from '../factories/userActivity-factory';
 import { cleanDb, generateValidToken } from '../helpers';
 
 beforeAll(async () => {
@@ -145,6 +144,180 @@ describe('GET /activity/user', () => {
           },
         },
       ]);
+    });
+  });
+});
+
+describe('POST /activity', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/activity');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/activity').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/activity').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 400 with a invalid body ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const body = { activityTypeId: 'error' };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.BAD_REQUEST);
+    });
+
+    it('should respond with status 403 when user has not a enrollment ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const activityType = await createActivityType();
+      const body = {
+        activityTypeId: activityType.id,
+      };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN);
+    });
+
+    it('should respond with status 403 when user has not a ticket ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const activityType = await createActivityType();
+      const body = {
+        activityTypeId: activityType.id,
+      };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN);
+    });
+
+    it('should respond with status 403 when user has not a payment ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.RESERVED);
+      const activityType = await createActivityType();
+      const body = {
+        activityTypeId: activityType.id,
+      };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN);
+    });
+
+    it('should respond with status 403 when user has a remote ticket ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeRemote();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const activityType = await createActivityType();
+      const body = {
+        activityTypeId: activityType.id,
+      };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN);
+    });
+
+    it('should respond with status 404 when has a invalidy activityType ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const body = { activityTypeId: 0 };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.NOT_FOUND);
+    });
+
+    it('should respond with status 403 when given activityType has no capacity ', async () => {
+      const fakeUser = await createUser();
+      const fakeEnrollment = await createEnrollmentWithAddress(fakeUser);
+      const fakeUser2 = await createUser();
+      const fakeEnrollment2 = await createEnrollmentWithAddress(fakeUser2);
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const activityType = await createActivityTypeWithFewVacancies();
+      await createActivity(activityType.id, fakeEnrollment.id);
+      await createActivity(activityType.id, fakeEnrollment2.id);
+      const body = { activityTypeId: activityType.id };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.FORBIDDEN);
+    });
+
+    it('should respond with status 409 when given activityType is already registered ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const activityType = await createActivityTypeWithFewVacancies();
+      const activity = await createActivity(activityType.id, enrollment.id);
+      const body = { activityTypeId: activityType.id };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.CONFLICT);
+    });
+
+    it('should respond with status 409 when given activityType has time conflict ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const activityType = await createActivityTypeWithFewVacancies();
+      const activity = await createActivity(activityType.id, enrollment.id);
+      const conflictActivityType = await createConflictActivityType(activityType.schedules, activityType.activityDate);
+      const body = { activityTypeId: conflictActivityType.id };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.CONFLICT);
+    });
+
+    it('should respond with status 201 with activity data ', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const activityType = await createActivityTypeWithFewVacancies();
+      const body = { activityTypeId: activityType.id };
+
+      const response = await server.post('/activity').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toEqual(httpStatus.CREATED);
     });
   });
 });
